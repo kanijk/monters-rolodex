ca1c8e1e1a7255a1dde23f1b5834b6f9650884a5
kanijkhandelwal <kanij@posistmail.com>
2020-04-25 13:32:57 +0530

    Updates

---
+++ precache-manifest.5d6418f7e2152e08b538456032f228ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d21f071ea2469dd81d6d24026f32de",
+    "url": "/monters-rolodex/index.html"
+  },
+  {
+    "revision": "9b28c568a1076d6ac5ec",
+    "url": "/monters-rolodex/static/css/main.5de17b86.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b28c568a1076d6ac5ec",
+    "url": "/monters-rolodex/static/js/main.d1792dfa.chunk.js"
+  },
+  {
+    "revision": "b81afad035c37d7b3ebd",
+    "url": "/monters-rolodex/static/js/runtime-main.4a752f61.js"
+  }
+]);
